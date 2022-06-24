8ff6b3254efaa68cf9ccde2364ec14c44c7e8346
Kwasi Dankwa <edankwa14@gmail.com>
2022-06-24 15:53:21 -0400

    Completed database models and installed dependecies

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,43 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    price:{
+      type: DataTypes.DECIMAL(10,2),
+      validate: {
+        isDecimal: true
+      },
+      allowNull: false,
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultvalue: 10,
+      validate: {
+        isNumeric: true
+      },
+
+    },
+
+    category_id:{
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id"
+      },
+    }
   },
   {
     sequelize,
